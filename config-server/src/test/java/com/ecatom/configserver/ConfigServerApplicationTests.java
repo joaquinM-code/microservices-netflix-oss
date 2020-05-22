1c1220e20a2a6266b8e742d4d9dbea746835babc
ecatom <netpassrecovers@hotmail.com>
2020-05-22 08:50:28 +0200

    Adding config server

---
+++ config-server/src/test/java/com/ecatom/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecatom.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
